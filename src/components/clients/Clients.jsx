4c6b23c117fc0cad99fc41af7067e7ddf4d6c2a9
adifkhan <pkadif@gmail.com>
2025-02-03 14:56:32 +0600

    worked on animations

--- src/components/clients/Clients.jsx
+++ src/components/clients/Clients.jsx
@@ -1,7 +1,7 @@
 import CircleAnimBottom from "./CircleAnimBottom";
 import CircleAnimTop from "./CircleAnimTop";
 import styles from "./clients.module.css";
-import CloudGallery from "./CloudGallery";
+// import CloudGallery from "./CloudGallery";
 
 const Clients = () => {
   return (
@@ -40,118 +40,118 @@ const Clients = () => {
 
 export default Clients;
 
-const entries = [
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Grain-and-Barrel-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.8,
-    alt: "Grain and Barrel",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/JetBlue-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.7,
-    alt: "JetBlue",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Bacardi-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Jim-Bean-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DribbleUp-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Davos-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/American-Airlines-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Stolen-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Delta-Airlines-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image:
-      "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Palm-Bay-International-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Tianma-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Pernod-Ricard-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Amtrak-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Park-Street-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DLTrading-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-  {
-    image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DLTrading-White.svg",
-    width: 120,
-    height: 120,
-    opacity: 0.6,
-    alt: "Scotch",
-  },
-];
+// const entries = [
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Grain-and-Barrel-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.8,
+//     alt: "Grain and Barrel",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/JetBlue-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.7,
+//     alt: "JetBlue",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Bacardi-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Jim-Bean-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DribbleUp-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Davos-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/American-Airlines-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Stolen-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Delta-Airlines-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image:
+//       "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Palm-Bay-International-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Tianma-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Pernod-Ricard-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Amtrak-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/Park-Street-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DLTrading-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+//   {
+//     image: "https://l5i.fa3.myftpupload.com/wp-content/uploads/2024/10/DLTrading-White.svg",
+//     width: 120,
+//     height: 120,
+//     opacity: 0.6,
+//     alt: "Scotch",
+//   },
+// ];
